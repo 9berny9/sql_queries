3f7705a82b8e907e6e63366b42e8b7d1aca14ce1
Michal B <96014046+9berny9@users.noreply.github.com>
2022-03-16 15:15:11 +0100

    Updated columns

--- Main.sql
+++ Main.sql
@@ -1,4 +1,4 @@
--- 1.   Rostou v pr?b?hu let mzdy ve v�ech odv?tv�ch, nebo v n?kter�ch klesaj�?
+-- 1.   Rostou v pr?b?hu let mzdy ve všech odv?tvích, nebo v n?kterých klesají?
 WITH salary_difference_by_year AS (
     SELECT
         date_year
@@ -39,7 +39,7 @@ FROM
     salary_difference_by_year 
 ;
 
--- 2.  Kolik je mo�n� si koupit litr? ml�ka a kilogram? chleba za prvn� a posledn� srovnateln� obdob� v dostupn�ch datech cen a mezd?
+-- 2.  Kolik je možné si koupit litr? mléka a kilogram? chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 WITH milk_bread_prices AS (
     SELECT
         date_year
@@ -90,7 +90,7 @@ FROM
     first_and_last_date 
 ;
 
--- 3.  KterÃ¡ kategorie potravin zdraÅ¾uje nejpomaleji (je u nÃ­ nejniÅ¾Å¡Ã­ percentuÃ¡lnÃ­ meziroÄ�nÃ­ nÃ¡rÅ¯st)?
+-- 3.  KterÃƒÂ¡ kategorie potravin zdraÃ…Â¾uje nejpomaleji (je u nÃƒÂ­ nejniÃ…Â¾Ã…Â¡ÃƒÂ­ percentuÃƒÂ¡lnÃƒÂ­ meziroÃ„ï¿½nÃƒÂ­ nÃƒÂ¡rÃ…Â¯st)?
 WITH price_difference_by_year AS (
     SELECT
         date_year 
@@ -138,7 +138,7 @@ ORDER BY
 ;
 
 
--- 4.Existuje rok, ve kterÃ©m byl meziroÄ�nÃ­ nÃ¡rÅ¯st cen potravin vÃ½raznÄ› vyÅ¡Å¡Ã­ neÅ¾ rÅ¯st mezd (vÄ›tÅ¡Ã­ neÅ¾ 10 %)?
+-- 4.Existuje rok, ve kterÃƒÂ©m byl meziroÃ„ï¿½nÃƒÂ­ nÃƒÂ¡rÃ…Â¯st cen potravin vÃƒÂ½raznÃ„â€º vyÃ…Â¡Ã…Â¡ÃƒÂ­ neÃ…Â¾ rÃ…Â¯st mezd (vÃ„â€ºtÃ…Â¡ÃƒÂ­ neÃ…Â¾ 10 %)?
 WITH food_and_salary_average AS (
     SELECT 
         date_year
@@ -187,6 +187,52 @@ SELECT *
 FROM difference_percent
 ;
 
-
+-- 5.  Má výška HDP vliv na změny ve mzdách a cenách potravin? 
+-- Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+WITH food_and_salary_average AS (
+    SELECT 
+        date_year
+        ,round(avg(average_gross_salary),0) AS average_gross_salary  
+        ,round(avg(average_food_price),2) AS average_food_price
+        ,GDP
+    FROM t_michal_bernatik_project_SQL_primary_final tmbpspf
+    WHERE food_category_code IS NOT NULL
+    GROUP BY date_year
+    ORDER BY food_category_code, date_year
+)
+,
+difference_by_year AS (
+    SELECT
+        *
+        ,average_gross_salary - lag(average_gross_salary) OVER 
+            (
+                ORDER BY date_year ASC
+                    ) AS salary_growth_by_year 
+        ,average_food_price - lag(average_food_price) OVER 
+            (
+                ORDER BY date_year ASC
+                    ) AS food_price_growth_by_year
+        ,GDP - lag(GDP) OVER 
+            (
+                ORDER BY date_year ASC
+                    ) AS GDP_growth_by_year
+    FROM
+        food_and_salary_average
+)
+,
+difference_percent AS (
+    SELECT
+        *
+        , round( ((100 * salary_growth_by_year) / average_gross_salary), 1) AS salary_percent_growth
+        , round( ((100 * food_price_growth_by_year) / average_food_price), 1) AS food_percent_growth
+        , round( ((100 * GDP_growth_by_year) / GDP), 1) AS GDP_percent_growth
+    FROM
+        difference_by_year
+)
+SELECT date_year 
+    ,salary_percent_growth
+    ,food_percent_growth
+    ,GDP_percent_growth 
+FROM difference_percent ;
 
 
