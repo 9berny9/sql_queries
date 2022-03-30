e956e175abdf97ee2777be728d2e1735a4383e23
Michal B <96014046+9berny9@users.noreply.github.com>
2022-03-30 20:50:33 +0200

    all answers ready for analysis

--- main_answers.sql
+++ main_answers.sql
@@ -136,3 +136,101 @@ GROUP BY
 ORDER BY
     average_percent_growth ASC
 ;
+
+-- 4. Mezirocni procentualni narust nebo klesani prumerne vyplaty vsech odvetvi a ceny konkretni potraviny serazenych podle roku.
+--    Pokud je procentualni narust jedne z velicin vetsi nebo mensi jak 10, tak upozoroni na vyraznou zmenu
+WITH food_and_salary_average AS (
+    SELECT 
+        date_year
+        ,food_category_code 
+        ,food_name
+        ,round(avg(average_gross_salary),0) AS average_gross_salary  
+        ,round(avg(average_food_price),2) AS average_food_price
+    FROM t_michal_bernatik_project_SQL_primary_final tmbpspf
+    WHERE food_category_code IS NOT NULL
+    GROUP BY date_year, food_category_code 
+    ORDER BY food_category_code, date_year
+)
+,
+difference_by_year AS (
+    SELECT
+        *
+        ,average_gross_salary - lag(average_gross_salary) OVER 
+            (
+            PARTITION BY food_category_code 
+                ORDER BY food_category_code , date_year ASC
+                    ) AS salary_growth_by_year 
+        ,average_food_price - lag(average_food_price) OVER 
+            (
+            PARTITION BY food_category_code
+                ORDER BY food_category_code, date_year ASC
+                    ) AS food_price_growth_by_year
+    FROM
+        food_and_salary_average
+)
+,
+difference_percent AS (
+    SELECT
+        *
+        , round( ((100 * salary_growth_by_year) / average_gross_salary), 1) AS salary_percent_growth
+        , round( ((100 * food_price_growth_by_year) / average_food_price), 1) AS food_percent_growth
+    FROM
+        difference_by_year
+)
+SELECT *
+    ,
+    CASE
+        WHEN abs(food_percent_growth) - abs(salary_percent_growth) > 10  THEN 'significant'
+        WHEN food_percent_growth IS NULL THEN NULL
+        ELSE 'normal'
+    END AS significant_change_in_food
+FROM difference_percent
+;
+
+-- 5.  Mezirocni procentualni rust nebo klesani prumerne vyplaty vsech odvetvi, prumerne ceny vsech potravin a vysky HDP serazenych podle roku.
+WITH food_and_salary_average AS (
+    SELECT 
+        date_year
+        ,round(avg(average_gross_salary),0) AS average_gross_salary  
+        ,round(avg(average_food_price),2) AS average_food_price
+        ,GDP
+    FROM t_michal_bernatik_project_SQL_primary_final tmbpspf
+    WHERE food_category_code IS NOT NULL
+    GROUP BY date_year
+    ORDER BY food_category_code, date_year
+)
+,
+difference_by_year AS (
+    SELECT
+        *
+        ,average_gross_salary - lag(average_gross_salary) OVER 
+            (
+                ORDER BY date_year ASC
+                    ) AS salary_growth_by_year 
+        ,average_food_price - lag(average_food_price) OVER 
+            (
+                ORDER BY date_year ASC
+                    ) AS food_price_growth_by_year
+        ,GDP - lag(GDP) OVER 
+            (
+                ORDER BY date_year ASC
+                    ) AS GDP_growth_by_year
+    FROM
+        food_and_salary_average
+)
+,
+difference_percent AS (
+    SELECT
+        *
+        , round( ((100 * salary_growth_by_year) / average_gross_salary), 1) AS salary_percent_growth
+        , round( ((100 * food_price_growth_by_year) / average_food_price), 1) AS food_percent_growth
+        , round( ((100 * GDP_growth_by_year) / GDP), 1) AS GDP_percent_growth
+    FROM
+        difference_by_year
+)
+SELECT date_year 
+    ,salary_percent_growth
+    ,food_percent_growth
+    ,GDP_percent_growth 
+FROM difference_percent ;
+
