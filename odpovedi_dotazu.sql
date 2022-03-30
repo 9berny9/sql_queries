ecca8942c2f7338c26b256f8b7d55100d6327b5c
Michal B <96014046+9berny9@users.noreply.github.com>
2022-03-30 19:19:57 +0200

    reseni druheho dotazu
    Pro vybrané potraviny vypíše cenu pro první a poslední rok ze všech záznamů, cenu průměrné mzdy ze všech odvětví a počet kusů nebo množství za průměrnou mzdu

--- odpovedi_dotazu.sql
+++ odpovedi_dotazu.sql
@@ -38,3 +38,54 @@ SELECT
 FROM
     salary_difference_by_year 
 ;
+
+-- 2.  Pocet mnozstvi litru mleka a kilogramu chleba z prumerne vyplaty pro prvni a posledni zaznam
+WITH milk_bread_prices AS (
+    SELECT
+        date_year
+        , food_category_code
+        , food_name
+        , food_price_value
+        , food_price_unit
+        , round(avg(average_gross_salary),2) AS average_gross_salary
+        , average_food_price
+    FROM
+        t_michal_bernatik_project_SQL_primary_final tmb
+    WHERE
+        1 = 1
+        AND food_category_code LIKE '111301'
+        OR food_category_code LIKE '114201'
+    GROUP BY
+        food_category_code
+        , date_year
+    ORDER BY
+        date_year ASC
+)
+,
+first_and_last_date AS (
+    (
+    SELECT
+        *
+    FROM
+        milk_bread_prices
+    ORDER BY
+        date_year ASC
+    LIMIT 2
+    )
+UNION
+    (
+    SELECT
+            *
+    FROM
+            milk_bread_prices
+    ORDER BY
+            date_year DESC
+    LIMIT 2
+    )
+)
+SELECT
+    *
+    , round((average_gross_salary / average_food_price), 0) AS quantity_from_salary
+FROM
+    first_and_last_date 
+;
